3082c160870d6df9b54d873f8f288bf290ad62eb
p_craft <ef81sp@gmail.com>
2023-12-27 00:32:01 +0900

    tailwindを導入

--- src/components/RoomAnswerButton.vue
+++ src/components/RoomAnswerButton.vue
@@ -5,12 +5,10 @@ const props = defineProps<{
 defineEmits<{ click: [answer: string] }>()
 </script>
 <template>
-  <button @click="$emit('click', props.option)" class="button">{{ option }}</button>
+  <button
+    @click="$emit('click', props.option)"
+    class="text-center w-10 text-xl aspect-[63/88]"
+  >
+    {{ option }}
+  </button>
 </template>
-<style scoped>
-.button {
-  font-size: 2rem;
-  width: 3rem;
-  margin: 0 0.25rem;
-}
-</style>
---
+++ src/components/RoomParticipantCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from "vue"
+
+const props = defineProps<{
+  answer: string
+  isOpen: boolean
+}>()
+const answer = computed(() => {
+  if (props.answer === "") return ""
+  if (props.isOpen) return props.answer
+  return "?"
+})
+</script>
+
+<template>
+  <p
+    class="aspect-[63/88] text-5xl w-20 border-4 rounded-md border-solid border-black grid place-items-center"
+  >
+    {{ answer }}
+  </p>
+</template>
